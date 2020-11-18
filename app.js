1ff57bdac39d0359442501ebd1a9e36aa53d49c0
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-18 13:37:10 -0500

    Set up our boilerplate (require/use express, set up port, body parser, template engine, import routes, start server)

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.static('public'))
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+let routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("App listening on PORT " + PORT);
+});
