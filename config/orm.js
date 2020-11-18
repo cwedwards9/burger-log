918af3102153316d1cc6acd32692c76eddfca0f2
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-18 13:34:03 -0500

    Create methods that execute MySQL queries by the db models.

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+const connection = require("./connection");
+
+const orm = {
+    selectAll: function(tableInput, cb) {
+        const q = "SELECT * FROM ??";
+        connection.query(q, [tableInput], (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(tableInput, val, cb) {
+        const q = "INSERT INTO ?? (burger_name) VALUES ??";
+        connection.query(q, [tableInput, val], (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(tableInput, val, condition, cb) {
+        const q = "UPDATE ?? SET devoured = ?? WHERE id = ??";
+        connection.query(q, [tableInput, val, condition], (err, result) => {
+            if(err) throw err;
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
