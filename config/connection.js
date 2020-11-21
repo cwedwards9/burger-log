381d781c36550d903ec3ccf52f1274fe90d25920
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-21 15:49:19 -0500

    Hook up application with JawsDB using the JawsDB environmental variable.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    host: 3306,
-    database: "burgers_db"
-});
+let connection;
+
+// If the server contains the JAWSDB_URL environmental variable, it connects to the JawsDB database
+// Otherwise it falls back on the local database 'burgers_db'
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        host: 3306,
+        database: "burgers_db"
+    });
+};
 
 connection.connect(err => {
     if(err) {
